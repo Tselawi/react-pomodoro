11489d74f5d97bceebab8952fd22a3ccc9c553f1
Tariq <selawi.be@gmail.com>
2021-04-29 15:47:15 +0200

    docs

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./app.css";
-import Main from "./components/main-screen";
+import Main from "./src/components/main-screen";
 
 ReactDOM.render(<Main />, document.querySelector("#app"));
